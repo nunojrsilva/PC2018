246eb502053096491f71cf40b5ebddaff9c97491
Daniel Gonçalves Martins <daniel.martins50@gmail.com>
2018-05-29 17:30:24 +0100

    Add shell scripts for handling server

---
+++ Server/startClient2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Starts server
+echo "*create_account client2 123456" | telnet localhost 12345
+
---
+++ Server/startServer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Starts server
+echo "c(vectors2d) . c(creatures) . c(players) . c(state) . c(login_manager) . c(server) . server:start()." | erl
+
